af6b836c6a52cd912b1039ab4add6891f4954729
Ivan Khavantsev <ivan.khavantsev@gmail.com>
2023-11-25 18:20:16 +0600

    commit

--- src/TeacherBackPropagation.java
+++ src/TeacherBackPropagation.java
@@ -19,31 +19,32 @@ public class TeacherBackPropagation {
 
         Layer ol = nn.layers[nn.layers.length - 1]; // Выходной слой
 
+        // Сначала сюда пишем ошибки выходного слоя
         double[] errors = new double[ol.size];
         for (int i = 0; i < ol.size; i++) {
-            // Сначала ищем ошибки последнего-выходного слоя
             errors[i] = targets[i] - nn.layers[nn.layers.length - 1].neurons[i];
         }
 
         // Повторяем всё для каждого скрытого слоя
         double[] gradients;
         for (int nli = nn.layers.length - 2; nli >= 0; nli--) { // Next Layer Index
-            Layer nl = nn.layers[nli]; // New layer, следующий слой (в обратном порядке идём)
+            Layer nl = nn.layers[nli]; // Next layer, следующий слой (в обратном порядке идём)
             int cli = nli + 1; // Current Layer Index
             Layer cl = nn.layers[cli]; // Current layer, текущий слой
 
             gradients = new double[cl.size];
             for (int i = 0; i < cl.size; i++) {
+                // Вычисляем градиент по производной от функции активации
                 gradients[i] = errors[i] * derivative.apply(cl.neurons[i]);
             }
 
+            // Когда вычислили градиенты, ищем ошибки следующего слоя для следующей итерации цикла
             double[] errorsNext = new double[nl.size];
             for (int i = 0; i < nl.size; i++) {
                 for (int j = 0; j < cl.size; j++) {
                     errorsNext[i] += nl.weights[i][j] * errors[j];
                 }
             }
-
             errors = new double[nl.size];
             System.arraycopy(errorsNext, 0, errors, 0, nl.size);
 
@@ -54,8 +55,11 @@ public class TeacherBackPropagation {
                         state.deltaWeights[nli] = new double[cl.size][nl.size];
                     }
 
+                    // Изменение, которое нужно произвести в весе следующего слоя
                     double deltaWeight = gradients[i] * (nl.neurons[j] * learningRate) + (moment * state.deltaWeights[nli][i][j]);
+                    // Сохраняем все дельты в статус, для следующего раза
                     state.deltaWeights[nli][i][j] = deltaWeight;
+                    // Обновляем вес
                     nl.weights[j][i] = nl.weights[j][i] + deltaWeight;
                 }
             }
@@ -65,9 +69,12 @@ public class TeacherBackPropagation {
                 if (state.deltaBieses[cli] == null) {
                     state.deltaBieses[cli] = new double[cl.size];
                 }
-                double biasDelta = (gradients[i] * learningRate) + (moment * state.deltaBieses[cli][i]);
-                cl.biases[i] += biasDelta;
-                state.deltaBieses[cli][i] = biasDelta;
+                // Вычисляем дельту для байеса текущего слоя
+                double deltaBias = (gradients[i] * learningRate) + (moment * state.deltaBieses[cli][i]);
+                // Обновляем байес
+                cl.biases[i] += deltaBias;
+                // Сохраняем все дельты в статус, для следующего раза
+                state.deltaBieses[cli][i] = deltaBias;
             }
         }
     }
